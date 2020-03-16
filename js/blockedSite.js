fd84ea9b855d4d161c3ea213ebd54716adeccd15
lgg <fed777os@gmail.com>
2020-03-16 19:13:36 +0300

    clear code

--- js/blockedSite.js
+++ js/blockedSite.js
@@ -8,40 +8,42 @@ var i = 15;
 var interval = 0;
 
 function updateCountdown() {
-  content.text("Unlisting " + site + " in " + --i + " seconds...");
-  if (i == 0) {
-    clearInterval(interval);
-    chrome.tabs.getCurrent(function(tab) {
-      chrome.extension.getBackgroundPage().unlistSite(tab.id, site);
-    });
-    window.location = site;
-  }
+    content.text("Unlisting " + site + " in " + --i + " seconds...");
+    if (i === 0) {
+        clearInterval(interval);
+        chrome.tabs.getCurrent(function (tab) {
+            chrome.extension.getBackgroundPage().unlistSite(tab.id, site);
+        });
+        window.location = site;
+    }
 }
 
 function beginCountdown() {
-  i = 15;
-  content.text("Unlisting " + site + " in " + i + " seconds...");
-  interval = setInterval(function() {updateCountdown(i)}, 1000);
+    i = 15;
+    content.text("Unlisting " + site + " in " + i + " seconds...");
+    interval = setInterval(function () {
+        updateCountdown(i)
+    }, 1000);
 }
 
 function modalHidden() {
-  clearInterval(interval);
+    clearInterval(interval);
 }
 
 function hideModal() {
-  $("#unlistModal").modal("hide");
+    $("#unlistModal").modal("hide");
 }
 
 $("#unlistModal").on('hidden', modalHidden);
 $("#cancelUnlist").click(hideModal);
 
 chrome.extension.onMessage.addListener(
-    function(message, sender, sendResponse) {
-  chrome.tabs.getCurrent(function(tab) {
-    if (tab.id == message) {
-      $("#unlistModal").modal("show");
-      beginCountdown();
-    }
-  });
-});
+    function (message, sender, sendResponse) {
+        chrome.tabs.getCurrent(function (tab) {
+            if (tab.id == message) {
+                $("#unlistModal").modal("show");
+                beginCountdown();
+            }
+        });
+    });
 
